8e2abfc3f9c4f0fff5e17a4baa3d8fb3c3cefe0b
Situ04-hub <aniketdas162005@gmail.com>
2026-02-27 18:18:46 +0530

    updated allowed hosts

--- tscvs/settings.py
+++ tscvs/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^8oat2aozql9*z@n=0c*vj$vrvqlx2a*c3zw0c2%a-h^8g1ehq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://title-verification-qied.onrender.com']
 
 
 # Application definition
